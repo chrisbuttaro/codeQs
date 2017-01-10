400995ba63c3808f49491d69a6c63f14285dfab1
chrisbuttaro <chris.buttaro@gmail.com>
2017-01-10 12:45:14 -0700

    login form created

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/CodeQs/app_client/components/loginComponent.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/CodeQs/app_client/components/loginComponent.js
@@ -6,6 +6,20 @@ app.component('loginComponent', {
   
     },
     
-    template: `<h1>login</h1>`
+    template: `<h1>CodeQs</h1>
+    <br>
+    <form class="col-md-12">
+    <div class="form-group">
+        <input type="text" class="form-control input-lg" placeholder="Username">
+    </div>
+    <div class="form-group">
+        <input type="password" class="form-control input-lg" placeholder="Password">
+    </div>
+  
+    <div class="form-group">
+        <button class="btn btn-primary btn-lg btn-block">Sign In</button>
+        <span class="pull-right"><a href="#">New Registration</a></span>
+    </div>
+</form>`
    
       });
