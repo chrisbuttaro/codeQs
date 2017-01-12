d7fc7e1b05c6ee1753c310b761273f77517efb55
Jonathan Edwards <edwardjd@colorado.edu>
2017-01-12 09:38:48 -0700

    pulling from chris

--- CodeQJPA/src/entities/ExamQuestion.java
+++ CodeQJPA/src/entities/ExamQuestion.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -13,6 +15,19 @@ public class ExamQuestion {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	// exam mapping
+	@ManyToOne
+	@JoinColumn(name="test_id")
+	private Exam exam;
+	
+	@ManyToOne
+	@JoinColumn(name="question_id")
+	private Question question;
+	
+	
+	
+	
 	@Column(name="is_right")
 	private boolean isRight;
 
