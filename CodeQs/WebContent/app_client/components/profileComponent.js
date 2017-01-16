68958ebb338be6a6aaea791a59f983fe62fc4dff
Jonathan Edwards <edwardjd@colorado.edu>
2017-01-15 17:19:12 -0700

    separate lists for java category and javascript category, hides them when their list is empty

--- CodeQs/WebContent/app_client/components/profileComponent.js
+++ CodeQs/WebContent/app_client/components/profileComponent.js
@@ -7,14 +7,17 @@ app.component('profileComponent', {
 	      vm.currentUser=authenticationService.currentUser;
 	    
 	 	    vm.data=[]
+	 	    vm.data2=[]
 	 	    
 	 	    vm.getAllExamsTaken = function(){
-	 	        profileService.getAllExamsTaken(2,1)
+	 	        profileService.getAllExamsTaken(vm.currentUser().id,1)
 	 	          .then(function(response){
 	 	            vm.data = response.data;
-	 	    	    console.log(response.data);
-	 
 	 	          });
+	 	        profileService.getAllExamsTaken(vm.currentUser().id,2)
+	 	        	.then(function(response) {
+	 	        		vm.data2 = response.data;
+	 	        	});
 	 	    }
 	 	    vm.getAllExamsTaken();
 	 	    console.log(vm.data);
@@ -26,8 +29,17 @@ app.component('profileComponent', {
 	      
 	    	<h1>{{$ctrl.currentUser().name}} Profile</h1>
 	    	<h3>Tests Taken</h3>
-	      <ul>
-	      <li ng-repeat="exam in $ctrl.data">exam id: {{exam.id}}, category: {{exam.category.name}} </li>
-	      </ul>
+	    	<div ng-hide="!$ctrl.data.length">
+	    	  <h4>Java</h4>	
+		      <ul ng-if="$ctrl.data">
+		    	<li ng-repeat="exam in $ctrl.data">exam id: {{exam.id}}, category: {{exam.category.name}} </li>
+		      </ul>
+	      </div>
+	      <div ng-hide="!$ctrl.data2.length">
+	    	   <h4>JavaScript</h4>
+			   <ul>
+		    	<li ng-repeat="exam in $ctrl.data2">exam id: {{exam.id}}, category: {{exam.category.name}} </li>
+			   </ul>
+		   </div>
 	 `
 	  }); 
