a06a6ba62b4dca9dd9a8c6618f7e786185f181c1
chrisbuttaro <chris.buttaro@gmail.com>
2017-01-17 08:22:16 -0700

    limited tests to 10 questions

--- CodeQs/WebContent/app_client/components/resultsComponent.js
+++ CodeQs/WebContent/app_client/components/resultsComponent.js
@@ -24,26 +24,24 @@ app.component('resultsComponent', {
 	 	        resultsService.getResultsByExamId(testService.examId)
 	 	          .then(function(response){
 	 	            vm.examQs = response.data;
-	 	             //vm.getScore; 
 	 	          });
 	 	    }
 	 	    vm.getResultsByExamId();
 	 
 	   },
 	      
+	  
 	    template : `
-	    <div class="container">
-	    	<h1>Test Results</h1>
-		    <h4>Score: {{$ctrl.getScore()}}%</h4>
-		    <div ng-repeat="examQ in $ctrl.examQs" ng-init="outerIndex = $index">
-		        <b>{{$index+1}}. {{examQ.question.question}}</b>
-		        <div ng-if="!examQ.right" ng-init="$ctrl.updateCounter()"></div>
-		        <div ng-class="examQ.right ? '.alert alert-success' : '.alert alert-danger'">
-		            <div ng-repeat="answer in examQ.question.answers">
-		                {{answer.answer}}
-		            </div>
-		        </div>
-		    </div>
-		</div>
+	     <div>Score: {{ $ctrl.getScore()}}%</div>
+	    	<table>
+       <tr ng-repeat="examQ in $ctrl.examQs" ng-init="outerIndex = $index">
+         <td><b>{{$index+1}}. {{examQ.question.question}}</b><div ng-if=!examQ.right ng-init="$ctrl.updateCounter()">X</div>
+          <div ng-repeat="answer in examQ.question.answers">
+          <div>{{answer.answer}}</div>
+          </div>
+           <br>
+       </tr>
+     </table>
+	    	 
 	 `
 	  }); 
